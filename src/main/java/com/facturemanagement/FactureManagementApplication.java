69b1ce3cecfcf6b47b1bbf024a9609e3a59cf196
Alejandro Fernández <108556384+AlejFernandezV@users.noreply.github.com>
2024-05-20 19:40:26 -0500

    first commit

---
+++ src/main/java/com/facturemanagement/FactureManagementApplication.java
@@ -0,0 +1,13 @@
+package com.facturemanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FactureManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FactureManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/facturemanagement/FactureManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.facturemanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FactureManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
