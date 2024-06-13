ba0ded69f8473f0007b9b017dba032ba20659c7a
JulianRuano <julianrmajin@gmail.com>
2024-06-12 22:54:39 -0500

    chore: Update FacturePersistenceAdapter to save facture products

--- src/main/java/com/facturemanagement/infraestructure/adapters/output/persistence/FacturePersistenceAdapter.java
+++ src/main/java/com/facturemanagement/infraestructure/adapters/output/persistence/FacturePersistenceAdapter.java
@@ -1,6 +1,8 @@
 package com.facturemanagement.infraestructure.adapters.output.persistence;
 
 import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -8,13 +10,16 @@ import org.springframework.data.domain.Pageable;
 import com.facturemanagement.application.ports.output.FactureCreatedOutputPort;
 import com.facturemanagement.application.ports.output.FactureGetOutputPort;
 import com.facturemanagement.domain.model.Facture;
+import com.facturemanagement.domain.model.Product;
 import com.facturemanagement.infraestructure.adapters.output.persistence.entity.FactureEntity;
+import com.facturemanagement.infraestructure.adapters.output.persistence.entity.ProductEntity;
 import com.facturemanagement.infraestructure.adapters.output.persistence.mapper.FacturePersistenceMapper;
 import com.facturemanagement.infraestructure.adapters.output.persistence.repository.FactureRepository;
 import com.facturemanagement.infraestructure.adapters.output.persistence.repository.ProductRepository;
 
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
@@ -29,15 +34,24 @@ public class FacturePersistenceAdapter implements FactureCreatedOutputPort, Fact
     private final FacturePersistenceMapper facturePersistenceMapper;
 
     @Override
+    @Transactional
     public Facture saveFacture(Facture facture) {
-
         System.out.println("Entrando a saveFacture");
 
-        if(facture.getFactProducts().isEmpty()){
+        if (facture.getFactProducts().isEmpty()) {
             return null;
         }
 
         FactureEntity factureEntity = this.facturePersistenceMapper.toFactureEntity(facture);
+
+        Set<Long> productIds = facture.getFactProducts().stream()
+                                      .map(Product::getProductId)
+                                      .collect(Collectors.toSet());
+        Set<ProductEntity> products = productRepository.findAllById(productIds).stream().collect(Collectors.toSet());
+
+        factureEntity.getFactProducts().clear();
+        factureEntity.getFactProducts().addAll(products);
+
 
         factureRepository.save(factureEntity);
 
