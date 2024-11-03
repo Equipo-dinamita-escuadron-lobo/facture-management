551744e83efbc4535638f28f9be7efc82ffdefe2
Parzivak <bherrera@unicauca.edu.co>
2024-11-02 20:14:57 -0500

    cambio en ruta qr Image

--- src/main/java/com/facturemanagement/infraestructure/adapters/output/PDFgeneration/FacturePDFAdapter.java
+++ src/main/java/com/facturemanagement/infraestructure/adapters/output/PDFgeneration/FacturePDFAdapter.java
@@ -576,7 +576,7 @@ public class FacturePDFAdapter implements FactureGeneratePDFOutputPort {
 
                         // Generar la imagen del código QR y agregarla en la segunda columna
                         Image qrCodeImage = generateQRCodeImage(
-                                        "http://contables.unicauca.edu.co/#/general/operations/invoice/facturaQR/" + facture.getFactId()); // Cambiar
+                                        "http://contables.unicauca.edu.co/#/general/facturaQR/3" + facture.getFactId()); // Cambiar
                         
 
                         table.addCell(new Cell().add(qrCodeImage.setHorizontalAlignment(HorizontalAlignment.CENTER)));
