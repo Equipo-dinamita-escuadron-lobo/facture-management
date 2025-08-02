f73e330e27b4c22051afe1e48fac04493b517cc7
JulianRuano <julianrmajin@gmail.com>
2025-08-02 17:51:24 -0500

    feat: remove @CrossOrigin annotations from REST controllers

--- src/main/java/com/facturemanagement/infraestructure/adapters/input/rest/FactureRestAdapter.java
+++ src/main/java/com/facturemanagement/infraestructure/adapters/input/rest/FactureRestAdapter.java
@@ -10,7 +10,6 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.facturemanagement.application.ports.input.CreateFactureUseCase;
@@ -39,7 +38,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@CrossOrigin("*")
 @RestController
 @RequestMapping("/api/factures/")
 @RequiredArgsConstructor
