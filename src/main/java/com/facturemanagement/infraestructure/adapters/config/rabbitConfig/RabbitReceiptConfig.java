f365298b8cfd97b10ff5769e83e1c87741936554
Juliana2101 <128009669+Juliana2101@users.noreply.github.com>
2025-10-30 13:22:27 -0500

    Adding a queue for accounting

--- src/main/java/com/facturemanagement/infraestructure/adapters/config/rabbitConfig/RabbitReceiptConfig.java
+++ src/main/java/com/facturemanagement/infraestructure/adapters/config/rabbitConfig/RabbitReceiptConfig.java
@@ -18,6 +18,7 @@ public class RabbitReceiptConfig {
     //Constants for invoice
     public static final String INVOICE_EXCHANGE = "invoice.exchange";
     public static final String INVOICE_PAYMENTS_QUEUE = "invoice.payments.queue";
+    public static final String INVOICE_ACCOUNTING_QUEUE = "invoice.accounting.queue";
 
     // Primary and message exchange dead for third invoices
     @Bean
@@ -33,5 +34,15 @@ public class RabbitReceiptConfig {
     @Bean
     Binding invoicePaymentsBinding() {
         return BindingBuilder.bind(invoicePaymentsQueue()).to(invoiceExchange());
+    }
+
+    @Bean
+    Queue invoiceAccountingQueue() {
+        return QueueBuilder.durable(INVOICE_ACCOUNTING_QUEUE).build();
+    }
+
+    @Bean
+    Binding invoiceAccountingBinding() {
+        return BindingBuilder.bind(invoiceAccountingQueue()).to(invoiceExchange());
     }
 }
